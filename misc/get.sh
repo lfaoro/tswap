32405cd44fac609156a48768f664eb9edb6eea90
user <3834345+lfaoro@users.noreply.github.com>
2025-02-02 14:04:49 +0200

    misc: refactor

--- misc/get.sh
+++ misc/get.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright (c) 2024 Leonardo Faoro. All rights reserved.
+# Use of this source code is governed by the AGPL-3.0 license
+# found in the LICENSE file.
+
+set -e
+
+echo "Hello, World! (coming soon)"
