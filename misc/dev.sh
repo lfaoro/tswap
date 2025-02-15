6d0502c931f24e0c8f4414d07be782b1f12685ae
user <me@leonardofaoro.com>
2025-02-15 17:14:56 +0200

    misc: dev.sh fix, readme misc: dev.sh fix

--- misc/dev.sh
+++ misc/dev.sh
@@ -2,21 +2,26 @@
 echo $$ > /tmp/troca_dev.pid
 cleanup() {
     echo "Ctrl+C detected. Cleaning up..."
+    pkill -TERM -x inotifywait
+    pkill -TERM -x troca
     pkill -P $$
     rm -f /tmp/troca_dev.pid
     exit 0
 }
-watchkill() {
-    while true; do
-    inotifywait -e modify $(find ./app -name '*.go') || {
-        pkill -x troca || true
-    }
+trap cleanup SIGINT SIGTERM
+start_troca(){
+    export TERM=xterm-256color
+    echo "starting troca"
+    go run -ldflags="-X main.APIURL=localhost:8080" ./cmd/troca --debug
+}
+
+start_tracker(){
+    inotifywait -r -m -e modify ./app --include '\.go$' | while read -r event; do
+        echo "Detected modification: $event"
+        pkill -9 -x troca || true
+        sleep 0.2
     done
 }
-trap cleanup SIGINT SIGTERM
-(watchkill) &
 
-while true; do
-    export TERM=xterm-256color
-    go run -ldflags="-X main.APIURL=localhost:8080" ./cmd/troca --debug
-done
+(start_tracker)&
+while :;do start_troca; sleep 1; done
